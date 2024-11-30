76392a2ccf566995ceb4f8340b15fa3479850839
пон <a17521721@gmail.com>
2024-11-30 19:16:41 +0700

    added map

--- космический купец/космический купец.cpp
+++ космический купец/космический купец.cpp
@@ -1,4 +1,4 @@
-﻿ь  
+﻿
 #include <iostream>
 #include "Venera.h"
 using namespace std;
@@ -6,7 +6,7 @@ using namespace std;
 void clear()
 {
     std::cout << "\033[2J";     // очистка экрана
-    std::cout << "\033[0;0f";   // перемещение курсора в верхний левый уголs
+    std::cout << "\033[0;0f";   // перемещение курсора в верхний левый угол
 }
 
 int main()
