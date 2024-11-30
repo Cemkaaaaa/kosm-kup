4b95039cf09c3c49e5e59c312c4bbd31b51fe919
x86c0d <ropkuf@ithev.com>
2024-11-30 16:58:45 +0700

    clear

--- космический купец/космический купец.cpp
+++ космический купец/космический купец.cpp
@@ -1,6 +1,13 @@
 ﻿
 #include <iostream>
 using namespace std;
+
+void clear()
+{
+    std::cout << "\033[2J";     // очистка экрана
+    std::cout << "\033[0;0f";   // перемещение курсора в верхний левый угол
+}
+
 int main()
 {
   
