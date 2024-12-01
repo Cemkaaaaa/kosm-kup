d8299c15a1dfabb3a492610f32e233c132843312
Клементьев Денис <dnevnik2496@mail.ru>
2024-12-01 15:18:07 +0300

    map

--- космический купец/космический купец.cpp
+++ космический купец/космический купец.cpp
@@ -4,11 +4,7 @@
 #include "Jupiter.h"
 using namespace std;
 
-void clear()
-{
-    std::cout << "\033[2J";     // очистка экрана
-    std::cout << "\033[0;0f";   // перемещение курсора в верхний левый угол
-}
+
 
 int main()
 {
