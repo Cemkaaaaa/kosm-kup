da9bcc498e17fe4c445355e0fe748079c96833cd
пон <a17521721@gmail.com>
2024-12-08 19:14:12 +0700

    Init карта

--- космический купец/карта.cpp
+++ космический купец/карта.cpp
@@ -1,62 +1,147 @@
 #include <iostream>
 #include <vector>
+#include <conio.h>
+#include <windows.h>
+#include <map>
 using namespace std;
-class Point {
-public:
-    int x, y;
 
-    Point(int x = 0, int y = 0) : x(x), y(y) {}
-};
-//Map::movePoint(int dx, int dy){реализация}.cpp
-//class Map{void movePoint(int dx, int dy)}
-class Map {
+void clear()
+{
+    system("cls");
+}
+
+class Chunks {
 protected:
-    int width, height;
-    Point point;
-    std::vector<std::vector<bool>> obstacles;
-
+    int chX, chY;
 public:
-    Map(int w = 35, int h = 25) : width(w), height(h), point(0, 0) {
-        obstacles.resize(height, std::vector<bool>(width, false));
+    Chunks(int chX = 0, int chY = 0) : chX(chX), chY(chY) {}
+};
+
+class Player {
+protected:
+    int x, y;
+public:
+    Player(int x = 0, int y = 0) : x(x), y(y) {}
+};
+
+class Decorations {
+    vector<pair<int, int>> decoC1;
+public:
+    Decorations() {
+        decoC1 = {  };
     }
-
-    virtual ~Map() {}
-
-    void movePoint(int dx, int dy) {
-        int newX = point.x + dx;
-        int newY = point.y + dy;
-
-        if (newX >= 0 && newX < width && newY >= 0 && newY < height && !obstacles[newY][newX]) {
-            point.x = newX;
-            point.y = newY;
-        }
-    }
-
-    void addObstacle(int x, int y) {
-        if (x >= 0 && x < width && y >= 0 && y < height) {
-            obstacles[y][x] = true;
-        }
-    }
-
-    void printMap() {
-        for (int y = 0; y < height; ++y) {
-            for (int x = 0; x < width; ++x) {
-                if (x == point.x && y == point.y) {
-                    std::cout << "P ";
-                }
-                else if (obstacles[y][x]) {
-                    std::cout << "# ";
-                }
-                else {
-                    std::cout << ". ";
+    bool isDecoration(int x, int y, int choice) const {
+        switch (choice) {
+        case 1:
+            for (auto i : decoC1) {
+                if (i.first == y && i.second == x) {
+                    return true;
                 }
             }
-            std::cout << std::endl;
+            break;
+        }
+        return false;
+    }
+    void addRandomDeco() {
+        srand(time(NULL));
+        int rngY = rand() % 35;
+        int rngX = rand() % 25;
+        decoC1.push_back({ rngX,rngY });
+    }
+};
+
+class Obstacles {
+    vector<pair<int, int>> obstC;
+public:
+    Obstacles() {
+        obstC = { {1,1},{1,3},{2,1},{3,1},{3,2},{3,3},{2,3} };
+    }
+    bool isObstacle(int x, int y) const {
+        for (auto i : obstC) {
+            if (i.first == y && i.second == x) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+//Кинь генереацию автоматическую
+class Map : public Player, public Chunks, public Obstacles, public Decorations {
+public:
+    void printMap() {
+        clear();
+        for (int y = chY * 25; y < 25 + (chY * 25); ++y) {
+            for (int x = chX * 35; x < 35 + (chX * 35); ++x) {
+                if (x == this->x && y == this->y) {
+                    cout << "[]";
+                }
+                else if (isObstacle(x, y)) {
+                    cout << "##";
+                }
+                else if (isDecoration(x, y, 1)) {
+                    cout << "#!";
+                }
+                else if (isDecoration(x - 1, y, 1)) {
+                    cout << "11";
+                }
+                else if (isDecoration(x - 2, y, 1)) {
+                    cout << "!#";
+                }
+                else {
+                    cout << ". ";
+                }
+            }
+            cout << endl;
+        }
+    }
+
+    void movePlayer(int dx, int dy) {
+        int newX = x + dx;
+        int newY = y + dy;
+
+        if (isObstacle(newX, newY) == false) {
+            x = newX;
+            y = newY;
+            if (newX < 0) {
+                chX = newX / 35 - 1;
+            }
+            else {
+                chX = newX / 35;
+            }
+            if (newY < 0) {
+                chY = newY / 25 - 1;
+            }
+            else {
+                chY = newY / 25;
+            }
+        }
+    }
+
+    void initMap() {
+        addRandomDeco();
+        while (true) {
+            this->printMap();
+            char input = _getch();
+            switch (input) {
+            case 'd':
+                this->movePlayer(1, 0);
+                break;
+            case 's':
+                this->movePlayer(0, 1);
+                break;
+            case 'a':
+                this->movePlayer(-1, 0);
+                break;
+            case 'w':
+                this->movePlayer(0, -1);
+                break;
+            }
         }
     }
 };
-// 
-class CustomMap : public Map {
-public:
-    CustomMap(int w, int h) : Map(w, h) {}
-};
+
+int main()
+{
+    Map map;
+    map.initMap();
+}
