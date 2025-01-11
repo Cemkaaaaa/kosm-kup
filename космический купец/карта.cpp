b981f3c0f4c7b1db019c28b1001f03a431f0647d
nower <renottgod@gmail.com>
2025-01-11 20:49:31 +0700

    название твоего комита

--- космический купец/карта.cpp
+++ космический купец/карта.cpp
@@ -19,10 +19,14 @@ public:
     Chunks(int chX = 0, int chY = 0, int oldChX = 0, int oldChY = 0) : chX(chX), chY(chY), oldChX(oldChX), oldChY(oldChY) {}
 };
 
-
+class Player {
+protected:
+    int x, y;
+public:
+    Player(int x = 0, int y = 0) : x(x), y(y) {}
+};
 
 class Decorations {
-protected:
     vector<pair<int, int>> decoC1;
     vector<pair<int, int>> decoC2;
     vector<pair<int, pair<int, int>>> decoratedChunks;
@@ -30,10 +34,9 @@ protected:
     int seed;
 public:
     Decorations() {
-        nullChunks = { {0 ,0}, {0, 1}, {1, 0}, {1, 1} };
-        decoC1 = {};
-        decoC2 = {};
+        decoC1 = {  };
     }
+    // ne skip
     bool isDecoration(int x, int y, int choice) const {
         switch (choice) {
         case 1:
@@ -43,74 +46,39 @@ public:
                 }
             }
             break;
-        case 2:
-            for (auto i : decoC2) {
-                if (i.first == y && i.second == x) {
-                    return true;
-                }
-            }
-            break;
         }
         return false;
     }
-    void addRandomDeco(int x = 0, int y = 0, int choice = 1) {
-        int rngY = rand() % 5;
-        int rngX = rand() % 5;
-        switch (choice) {
-        case 1:
-            decoC1.push_back({ rngX + x * 7, rngY + y * 5 });
-            break;
-        case 2:
-            decoC2.push_back({ rngX + x * 7, rngY + y * 5 });
-            break;
-        }
-    }
-    void fillChunk() {
-        for (int y = 0; y < 7; y++) {
-            for (int x = 0; x < 5; x++) {
-                addRandomDeco(x, y, 1);
-                int chance = rand() % 10;
-                if (chance <= 4) {
-                    addRandomDeco(x, y, 2);
-                }
-            }
-        }
-    }
-    void addSeedToChs(int seed, int x, int y) {
-        decoratedChunks.push_back({ seed, {x, y} });
-    }
-    int getSeed(int x, int y) {
-        for (auto i : decoratedChunks) {
-            if (i.second.first == x && i.second.second == y) {
-                return i.first;
-            }
-        }
-        return 0;
-    }
-    void print() {
-        for (auto i : decoratedChunks) {
-            cout << i.first << " " << i.second.first << " " << i.second.second << endl;
-        }
-    }
-    void clearChunk() {
-        decoC1 = {};
-        decoC2 = {};
+    void addRandomDeco() {
+        srand(time(NULL));
+        int rngY = rand() % 35;
+        int rngX = rand() % 25;
+        decoC1.push_back({ rngX,rngY });
     }
 };
 
 class Obstacles {
     vector<pair<int, int>> obstC;
+    vector<pair<pair<int, int>, pair<int, int>>> largeObstC;
+    vector<pair<int, int>> deleteObstC;
+    vector<pair<pair<int, int>, pair<int, int>>> deleteLargeObstC;
 public:
     Obstacles() {
-        obstC = { {1,1},{1,3},{2,1},{3,1},{3,2},{3,3},{2,3} };
+        largeObstC = { {{4,3}, {12,15}} }; // коорды для больших препятствий
+        obstC = {}; // коорды для точечных препятствий
     }
     bool isObstacle(int x, int y) const {
         for (auto i : obstC) {
-            if (i.first == y && i.second == x) {
+            if (i.first == x && i.second == y) {
                 return true;
             }
         }
         return false;
+    }
+    void printObst() {
+        for (auto i : obstC) {
+            cout << i.first << ", " << i.second << "\n";
+        }
     }
 };
 class Map : public Player, public Chunks, public Obstacles, public Decorations {
@@ -120,43 +88,27 @@ public:
         clear();
         for (int y = chY * 25; y < 25 + (chY * 25); ++y) {
             for (int x = chX * 35; x < 35 + (chX * 35); ++x) {
-                // человечек
                 if (x == this->x && y == this->y) {
                     cout << "[]";
-                }
-                else if (x == this->x && y - 1 == this->y) {
-                    cout << "TT";
-                }
-                else if (x == this->x && y + 1 == this->y) {
-                    cout << "()";
                 }
                 else if (isObstacle(x, y)) {
                     cout << "##";
                 }
-                // Дерево
-                else if (isDecoration(x - chX * 35, y - chY * 25, 2)) { cout << "/#"; }
-                else if (isDecoration(x - 1 - chX * 35, y - chY * 25, 2)) { cout << "&#"; }
-                else if (isDecoration(x - 2 - chX * 35, y - chY * 25, 2)) { cout << "#\\"; }
-                else if (isDecoration(x - chX * 35, y - 1 - chY * 25, 2)) { cout << "#&"; }
-                else if (isDecoration(x - 1 - chX * 35, y - 1 - chY * 25, 2)) { cout << "##"; }
-                else if (isDecoration(x - 2 - chX * 35, y - 1 - chY * 25, 2)) { cout << "&#"; }
-                else if (isDecoration(x - chX * 35, y - 2 - chY * 25, 2)) { cout << "\\#"; }
-                else if (isDecoration(x - 1 - chX * 35, y - 2 - chY * 25, 2)) { cout << "#&"; }
-                else if (isDecoration(x - 2 - chX * 35, y - 2 - chY * 25, 2)) { cout << "#/"; }
-                else if (isDecoration(x - 1 - chX * 35, y - 3 - chY * 25, 2)) { cout << "||"; }
-                else if (isDecoration(x - 1 - chX * 35, y - 4 - chY * 25, 2)) { cout << "||"; }
-                // Трава
-                else if (isDecoration(x - chX * 35, y - chY * 25, 1)) { cout << ".,"; }
-                else if (isDecoration(x - 1 - chX * 35, y - chY * 25, 1)) { cout << "\"\""; }
-                else if (isDecoration(x - 2 - chX * 35, y - chY * 25, 1)) { cout << ",."; }
-                // Пустое 
+                else if (isDecoration(x, y, 1)) {
+                    cout << "#!";
+                }
+                else if (isDecoration(x - 1, y, 1)) {
+                    cout << "11";
+                }
+                else if (isDecoration(x - 2, y, 1)) {
+                    cout << "!#";
+                }
                 else {
                     cout << ". ";
                 }
             }
             cout << endl;
         }
-        cout << getSeed(chX, chY);
     }
 
     void movePlayer(int dx, int dy) {
@@ -168,17 +120,37 @@ public:
 
             x = newX;
             y = newY;
-            if (newX < 0) {
-                chX = newX / 35 - 1;
+            if (x < 0) {
+                chX = x / 35 - 1;
             }
             else {
-                chX = newX / 35;
+                chX = x / 35;
             }
-            if (newY < 0) {
-                chY = newY / 25 - 1;
+            if (y < 0) {
+                chY = y / 25 - 1;
             }
             else {
-                chY = newY / 25;
+                chY = y / 25;
+            }
+        }
+    }
+    void tick() {
+        if (oldChY != chY || oldChX != chX) {
+            clearChunk();
+            bool check = false;
+            for (auto i : nullChunks) {
+                if (chX == i.first && chY == i.second) {
+                    check = true;
+                }
+            }
+            if (!check) {
+                seed = getSeed(chX, chY);
+                if (seed == 0) {
+                    seed = rand();
+                    addSeedToChs(seed, chX, chY);
+                }
+                srand(seed);
+                fillChunk();
             }
         }
     }
@@ -205,24 +177,72 @@ public:
     }
     void initMap() {
         while (true) {
-            tick();
-            srand(seed);
             this->printMap();
             char input = _getch();
             switch (input) {
             case 'd':
-                this->movePlayer(1, 0);
+                movePlayer(1, 0);
                 break;
             case 's':
-                this->movePlayer(0, 1);
+                movePlayer(0, 1);
                 break;
             case 'a':
-                this->movePlayer(-1, 0);
+                movePlayer(-1, 0);
                 break;
             case 'w':
-                this->movePlayer(0, -1);
+                movePlayer(0, -1);
                 break;
             }
+        }
+    }
+    void printMap() {
+        clear();
+        for (int y = chY * 25; y < 25 + (chY * 25); ++y) {
+            for (int x = chX * 35; x < 35 + (chX * 35); ++x) {
+                // человечек
+                if (x == this->x && y == this->y) {
+                    cout << "[]";
+                }
+                else if (x == this->x && y - 1 == this->y) {
+                    cout << "TT";
+                }
+                else if (x == this->x && y + 1 == this->y) {
+                    cout << "()";
+                }
+                // Препятствие
+                else if (isObstacle(x, y)) {
+                    cout << "##";
+                }
+                // korobochka
+                else if (isDecoration(x - chX * 35, y - chY * 25, 3)) { cout << "88"; }
+                else if (isDecoration(x - 1 - chX * 35, y - chY * 25, 3)) { cout << "88"; }
+                else if (isDecoration(x - chX * 35, y - 1 - chY * 25, 3)) { cout << "88"; }
+                else if (isDecoration(x - 1 - chX * 35, y - 1 - chY * 25, 3)) { cout << "88"; }
+                // Дерево
+                else if (isDecoration(x - chX * 35, y - chY * 25, 2)) { cout << "/#"; }
+                else if (isDecoration(x - 1 - chX * 35, y - chY * 25, 2)) { cout << "&#"; }
+                else if (isDecoration(x - 2 - chX * 35, y - chY * 25, 2)) { cout << "#\\"; }
+                else if (isDecoration(x - chX * 35, y - 1 - chY * 25, 2)) { cout << "#&"; }
+                else if (isDecoration(x - 1 - chX * 35, y - 1 - chY * 25, 2)) { cout << "##"; }
+                else if (isDecoration(x - 2 - chX * 35, y - 1 - chY * 25, 2)) { cout << "&#"; }
+                else if (isDecoration(x - chX * 35, y - 2 - chY * 25, 2)) { cout << "\\#"; }
+                else if (isDecoration(x - 1 - chX * 35, y - 2 - chY * 25, 2)) { cout << "#&"; }
+                else if (isDecoration(x - 2 - chX * 35, y - 2 - chY * 25, 2)) { cout << "#/"; }
+                else if (isDecoration(x - 1 - chX * 35, y - 3 - chY * 25, 2)) { cout << "||"; }
+                else if (isDecoration(x - 1 - chX * 35, y - 4 - chY * 25, 2)) { cout << "||"; }
+                // venom
+                else if ((x > 5 && x < 10) && (y > 5 && y < 10)) { cout << "  "; }
+                else if ((x >= 5 && x <= 10) && (y >= 5 && y <= 10)) { cout << "[]"; }
+                // Трава
+                else if (isDecoration(x - chX * 35, y - chY * 25, 1)) { cout << ".,"; }
+                else if (isDecoration(x - 1 - chX * 35, y - chY * 25, 1)) { cout << "\"\""; }
+                else if (isDecoration(x - 2 - chX * 35, y - chY * 25, 1)) { cout << ",."; }
+                // Пустое 
+                else {
+                    cout << "  ";
+                }
+            }
+            cout << endl;
         }
     }
 };
